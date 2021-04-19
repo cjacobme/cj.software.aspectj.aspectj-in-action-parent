da961337e2c611263b539f61f696d806f96c3d1c
cjacobme <cjacobme@aol.com>
2021-04-19 20:19:25 +0200

    added some functionality

--- CJ - AspectJ - AspectJ in Action - Parent/ch09-spring/src/main/java/ch09/InventoryMain.java
+++ CJ - AspectJ - AspectJ in Action - Parent/ch09-spring/src/main/java/ch09/InventoryMain.java
@@ -1,13 +1,31 @@
 package ch09;
 
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import ch09.domain.Product;
+import ch09.service.InventoryService;
+import ch09.service.impl.InventoryServiceStubImpl;
 
 @SpringBootApplication
 public class InventoryMain
 {
 	public static void main(String[] args)
 	{
-		SpringApplication.run(InventoryMain.class, args);
+		AnnotationConfigApplicationContext ctx = new AnnotationConfigApplicationContext();
+		try
+		{
+			ctx.register(InventoryServiceStubImpl.class);
+			ctx.refresh();
+
+			Product myBook = new Product("CJ", "CJ experiments with Aspect-J", 44.99);
+			InventoryService inventoryService = (InventoryService) ctx.getBean("inventoryService");
+			inventoryService.addProduct(myBook, 100000);
+			inventoryService.removeProduct(myBook, 100000);
+		}
+		finally
+		{
+			ctx.close();
+		}
 	}
 }

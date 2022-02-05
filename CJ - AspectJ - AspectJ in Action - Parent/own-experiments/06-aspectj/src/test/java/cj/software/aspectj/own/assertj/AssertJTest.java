34f13ce1fba84e409edc9113229db6f4f9c22049
cjacobme <cjacobme@aol.com>
2022-02-05 12:30:23 +0100

    more tests with AssertJ

--- CJ - AspectJ - AspectJ in Action - Parent/own-experiments/06-aspectj/src/test/java/cj/software/aspectj/own/assertj/AssertJTest.java
+++ CJ - AspectJ - AspectJ in Action - Parent/own-experiments/06-aspectj/src/test/java/cj/software/aspectj/own/assertj/AssertJTest.java
@@ -4,14 +4,21 @@ import static org.assertj.core.api.Assertions.*;
 
 import org.junit.Test;
 
-public class SimpleTest
+public class AssertJTest
 {
 	@Test
 	public void thisWillFail()
 	{
 		int number = 1;
-		assertThat(number).as("number").isEqualTo(2);
+		assertThat(number).as("number %d", number).isEqualTo(2);
 		// all classes in package org.assertj.core
 		// and from these all methods which have names that start with "assert"
+	}
+
+	@Test
+	public void thisWillPass()
+	{
+		int number = 2;
+		assertThat(number).as("this will %s", "pass").isEqualByComparingTo(2);
 	}
 }

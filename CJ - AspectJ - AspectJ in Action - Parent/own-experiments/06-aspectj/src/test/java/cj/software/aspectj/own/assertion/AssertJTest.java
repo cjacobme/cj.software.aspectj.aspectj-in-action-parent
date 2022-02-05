febc34b543770293b3862c6131fa0a1c5b2f4e54
cjacobme <cjacobme@aol.com>
2022-02-05 13:09:37 +0100

    removed comment

--- CJ - AspectJ - AspectJ in Action - Parent/own-experiments/06-aspectj/src/test/java/cj/software/aspectj/own/assertion/AssertJTest.java
+++ CJ - AspectJ - AspectJ in Action - Parent/own-experiments/06-aspectj/src/test/java/cj/software/aspectj/own/assertion/AssertJTest.java
@@ -11,8 +11,6 @@ public class AssertJTest
 	{
 		int number = 1;
 		assertThat(number).as("number %d", number).isEqualTo(2);
-		// all classes in package org.assertj.core
-		// and from these all methods which have names that start with "assert"
 	}
 
 	@Test
